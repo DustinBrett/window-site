bc0a5fa8c5650c9a1a1e600487660f68aa7de4a5
Dustin Brett <dustinbrett@gmail.com>
2019-04-22 22:43:53 -0700

    Improvements

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
+/* TODO
+  - Account for DST in setTime
+*/
+
 const x = new Ventus.WindowManager();
 
 const createWindow = (title, x, y, width, height, query) => {
@@ -22,8 +26,16 @@ const exposeToggle = () => {
   window.x.mode = (window.x.mode === 'expose') ? 'default' : 'expose'
 };
 
+const TIMEZONE_OFFSET = -7;
+const MINUTE_MS = 60000;
+const HOUR_MS = MINUTE_MS * 60;
+
 const setTime = () => {
-  document.getElementById('time').innerHTML = new Date().toLocaleTimeString();
+  const now = new Date();
+  const utc = now.getTime() + (now.getTimezoneOffset() * MINUTE_MS);
+  const time = new Date(utc + (HOUR_MS * TIMEZONE_OFFSET)).toLocaleTimeString();
+
+  document.getElementById('time').innerHTML = time;
 };
 
 $('#blog')('click', () => createWindow('Blog', 150, 80, 630, 600, '#content'));
